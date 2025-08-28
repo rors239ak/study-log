ad987e4fd062bd9e99c6e3da43930acd120d4dd3
rors239ak <rors239ak@gmail.com>
2025-08-28 13:55:57 +0900

    exampleに変更

--- PyPDF2a.py
+++ PyPDF2a.py
@@ -3,9 +3,9 @@ from PyPDF2 import PdfMerger
 merger = PdfMerger()
 
 # 結合したいPDFファイルを追加
-merger.append("履歴書(平野　裕也).pdf")
-merger.append("職務経歴書(平野　裕也).pdf")
+merger.append("example1.pdf")
+merger.append("example2.pdf")
 
 # 出力ファイル
-merger.write("履歴書_職務経歴書(平野　裕也).pdf")
+merger.write("example1_2.pdf")
 merger.close()
