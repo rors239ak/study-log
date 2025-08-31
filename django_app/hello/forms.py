d1ef6a861d223d41c84bb999b0b9d2098de809ed
rors239ak <rors239ak@gmail.com>
2025-08-31 17:32:04 +0900

    バリデーション

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -5,4 +5,10 @@ class FindForm(forms.Form):
   find = forms.CharField(label="Find", required=False, widget=forms.TextInput(attrs={"class":"form-control"}))
 
 class CheckForm(forms.Form):
-  str = forms.CharField(label="Name", widget=forms.TextInput(attrs={"class":"form-control"}))
+  str = forms.CharField(label="String", widget=forms.TextInput(attrs={"class":"form-control"}))
+
+  def clean(self):
+    cleaned_data = super().clean()
+    str = cleaned_data["str"]
+    if (str.lower().startswith("no")):
+      raise forms.ValidationError("You input 'NO'!")
