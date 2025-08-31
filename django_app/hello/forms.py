38c28ff29559ae1edeea4508e28b6b482210a568
rors239ak <rors239ak@gmail.com>
2025-08-31 16:42:02 +0900

    バリデーション

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -3,3 +3,6 @@ from .models import Friend
 
 class FindForm(forms.Form):
   find = forms.CharField(label="Find", required=False, widget=forms.TextInput(attrs={"class":"form-control"}))
+
+class CheckForm(forms.Form):
+  str = forms.CharField(label="Name", widget=forms.TextInput(attrs={"class":"form-control"}))
